d07773e04e867c3ce9af5e637f8883031fa0a9af
abhishekk677 <25111cn058@glbitm.ac.in>
2026-03-27 12:11:47 +0530

    sum & avg in array.c

---
+++ sum & avg in array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int arr[] = {10, 20, 30, 40, 50};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int sum = 0;
+    float average;
+    int i;
+
+    for (i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+
+    average = (float)sum / n;
+
+    printf("Sum = %d\n", sum);
+    printf("Average = %.2f\n", average);
+
+    return 0;
+}
